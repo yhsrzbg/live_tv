1e7cfa1f026acbea4406ca738b0a70547e952b0a
yuehansong_gh <yuehansong@guangtech.com>
2026-03-05 16:04:27 +0800

    fix: add kotlin compose compiler plugin for kotlin 2

--- app-tv/build.gradle.kts
+++ app-tv/build.gradle.kts
@@ -3,6 +3,7 @@ import java.util.Properties
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("org.jetbrains.kotlin.plugin.compose")
     id("com.google.devtools.ksp")
 }
 
@@ -60,10 +61,6 @@ android {
 
     buildFeatures {
         compose = true
-    }
-
-    composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.15"
     }
 
     packaging {
